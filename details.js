6875e5a6c21cab32747480a768ef0e717e731410
CoderOp8 <102341742+CoderOp8@users.noreply.github.com>
2022-03-27 19:06:23 +0530

    Add files via upload

---
+++ details.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+    token: '5281162783:AAGAOJzLNI5fyvDSRGMhqVZzjX_aUNMv3d0',
+
+    admins: [1635788715],
+
+    curr:'INR',
+
+    keyb : 
+
+
+
+[
+
+    ['📔 Account'],
+
+
+
+    ['👫 Invite','🎁 Surprise','🗂️ Wallet'],
+
+
+
+    
+
+
+
+    ['💵 Withdraw','📈 Status']
+
+
+
+]
+
+}
