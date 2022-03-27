87c0fe8034b8046ff215acb98dbd868984360a2d
CoderOp8 <102341742+CoderOp8@users.noreply.github.com>
2022-03-27 19:08:06 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,33 +1,65 @@
-module.exports = {
-
-    token: '5281162783:AAGAOJzLNI5fyvDSRGMhqVZzjX_aUNMv3d0',
-
-    admins: [1635788715],
-
-    curr:'INR',
-
-    keyb : 
-
-
-
-[
-
-    ['📔 Account'],
-
-
-
-    ['👫 Invite','🎁 Surprise','🗂️ Wallet'],
-
-
-
-    
-
-
-
-    ['💵 Withdraw','📈 Status']
-
-
-
-]
-
+module.exports = {
+
+
+
+    token: '5156386404:AAEzt-gup6ey929XfLnafxG8vGJvhMNZv6k',
+
+
+
+    admins: [1635788715,1118504892],
+
+
+
+    curr:'INR',
+
+
+
+    keyb : 
+
+
+
+
+
+
+
+[
+
+
+
+    ['📔 Account'],
+
+
+
+
+
+
+
+    ['👫 Invite','🎁 Surprise','🗂️ Wallet'],
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    ['💵 Withdraw','📈 Status']
+
+
+
+
+
+
+
+]
+
+
+
 }
